480062635d26676a48a9ab57e86d3a70260f07f0
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2022-03-08 09:55:29 +0700

    add soal-7

---
+++ soal-7.js
@@ -0,0 +1,35 @@
+const dataPenjualanPakAldi = [
+  {
+    namaProduct : 'Sepatu Futsal Nike Vapor Academy 8',
+    hargaSatuan: 760000,
+    kategori : "Sepatu Sport",
+    totalTerjual : 90,
+  },
+  {
+    namaProduct : 'Sepatu Warrior Tristan Black Brown High',
+    hargaSatuan: 960000,
+    kategori : "Sepatu Sneaker",
+    totalTerjual : 37,
+  },
+  {
+    namaProduct : 'Sepatu Warrior Tristan Maroon High ',
+    kategori : "Sepatu Sneaker",
+    hargaSatuan: 360000,
+    totalTerjual : 90,
+  },
+  {
+    namaProduct : 'Sepatu Warrior Rainbow Tosca Corduroy',
+    hargaSatuan: 120000,
+    kategori : "Sepatu Sneaker",
+    totalTerjual : 90,
+  }
+]
+
+function hitungTotalPenjualan(dataPenjualan){
+  let result = dataPenjualan.reduce((prev,cur) => prev+cur.totalTerjual, 0)
+
+  return result
+}
+
+console.log(hitungTotalPenjualan(dataPenjualanPakAldi)) // => 307
+
